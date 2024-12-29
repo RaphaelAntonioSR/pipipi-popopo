e48f58e4298171bf9c1225287e49fe8531bab7f4
RaphaelAntonioSR <raphael.asrangel@gmail.com>
2024-12-29 04:09:59 -0400

    first commit

---
+++ setup/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from galeria.views import index
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('galeria.urls')),
+]
